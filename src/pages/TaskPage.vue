4f7d932d0faf64743c89c41c13b9cf2fe5a77e48
azrirafli72 <m.rafli.azriansyah@gmail.com>
2024-07-01 00:50:03 +0700

    Perubahan Router

--- src/pages/TaskPage.vue
+++ src/pages/TaskPage.vue
@@ -12,10 +12,10 @@
   const taskUrl = computed(() => {
     const taskId = route.params.id;
     const urls = {
-      1: 'https://github.com/azrirafli72/tugas1-pbk-223510644.git?authuser=0',
+      1: 'https://azri-project-cv.vercel.app/',
       2: 'https://m-rafli-azriansyah-223510644-4b-pbkpraktikum-tugas2.vercel.app/',
       3: 'http://m-rafli-azriansyah-223510644-tugas3.vercel.app/?authuser=0',
-      4: 'https://youtube.com',
+      4: 'https://m-rafli-azriansyah-223510644-tugas4-prak-pbk.vercel.app/?authuser=0',
       5: 'https://m-rafli-azriansyah-223510644-tugas5.vercel.app/?authuser=0',
       6: 'https://m-rafli-azriansyah-223510644-tugas6.vercel.app/?authuser=0',
       7: 'https://m-rafli-azriansyah-223510644-tugas7.vercel.app/?authuser=0',
